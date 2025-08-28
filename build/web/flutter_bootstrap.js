6410b7ddcff133ed4e8491bde08c781099f50854
Mahmoud_Elnagar <elnagarm852@gmail.com>
2025-08-29 01:35:22 +0300

    update icon

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2699269658"
+    serviceWorkerVersion: "1689084688"
   }
 });

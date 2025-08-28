0b92c6d40249f5838899c28c557f2c3257ac6b82
Mahmoud_Elnagar <elnagarm852@gmail.com>
2025-08-29 02:53:25 +0300

    updated

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1402070018"
+    serviceWorkerVersion: "3329174290"
   }
 });
